7b4ca796e9c91820a8e88ffa2acfc0d0b2536790
Amanda Alves Vieira Furtado <amandafurtado@Amandas-MBP.Home>
2022-11-10 17:45:07 +0000

    project initialized with springboot

---
+++ src/test/java/com/ecommerce/backendamanda/BackendAmandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.backendamanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAmandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
